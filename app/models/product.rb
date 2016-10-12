1be5dd8bc5a235541f1622882d288a1efa45a3d9
Seamus Delaney <s.odubhshlaine1@nuigalway.ie>
2016-10-12 11:22:14 +0100

    added cart and line item models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, presence: true
 	validates :title, uniqueness: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -6,4 +8,17 @@ class Product < ActiveRecord::Base
 	  with: %r{\.(gif|jpg|jpeg|png)\Z}i,
 	  message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	has_many :line_items
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
 end
