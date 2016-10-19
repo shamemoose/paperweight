0c41d9c3d83ea653d1b7f024462abc6ce70f7e1c
Seamus Delaney <s.odubhshlaine1@nuigalway.ie>
2016-10-19 11:19:38 +0100

    Added database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019095913) do
+ActiveRecord::Schema.define(version: 20161019100744) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
